de1f62055b4151354230d131f08768c7acb31882
InvincibleRMC <rmc@carlstrom.com>
2020-09-01 12:55:36 -0400

    led blink code

---
+++ Module 1/Lab 1/LED_Blinking.m
@@ -0,0 +1,13 @@
+clear;
+close all;
+clc;
+
+%Creates arduino object
+a = arduino();
+
+%blinks arduino 10 times 
+for i=i:10
+    writeDigitalPin(a,"D8",1);
+    pause(.5);
+    writeDigitalPin(a,"D8",0);
+end
